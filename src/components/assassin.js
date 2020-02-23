d282eec8a2589598f7670016c1306e6f64ba7aac
chengle22 <chen3239@colorado.edu>
2020-02-22 19:29:32 -0700

    this is our navigation menu super unpolished

--- src/components/assassin.js
+++ src/components/assassin.js
@@ -1,22 +1,94 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import * as React from 'react';
+import { Text, TextInput, View, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-class TestMenu extends Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text>Game</Text>
-			</View>
-		);
-	}
+function HomeScreen({ navigation, route }) {
+
+
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'seagreen'}}>
+    <View style ={styles.buttoncontainer}>
+          <Button
+            title="Press Me"
+            onPress={() => navigation.navigate('CreatePost')}
+            color = "black"
+            backgroundColor = "black"
+          />
+    </View>
+    <View style ={styles.buttoncontainer}>
+          <Button
+            title="Press Me"
+            color = "black"
+            onPress={() => navigation.navigate('CreatePost')}
+          />
+    </View>
+    <View style ={styles.buttoncontainer}>
+          <Button
+            title="This looks great!"
+            onPress={() => navigation.navigate('CreatePost')}
+            color = "black"
+          />
+    </View>
+    <View style ={styles.buttoncontainer}>
+          <Button
+            title="OK!"
+            color = "black"
+            onPress={() => navigation.navigate('CreatePost')}
+          />
+    </View>
+    <View style={styles.buttoncontainer}>
+      <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+        color = "black"
+      />
+    </View>
+    </View>
+  );
 }
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
+	buttoncontainer: {
+		margin: 20,
 	}
 });
 
-export default TestMenu;
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'black' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          navigation.navigate('Home', { post: postText });
+        }}
+      />
+    </>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator   screenOptions={{
+    headerShown: false
+  }}>
+      
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
